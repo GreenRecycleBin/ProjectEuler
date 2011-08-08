63d747c0115d3661094a59d663f87dc41a88ca1f
Daniel Le <GreenRecycleBin@gmail.com>
2011-08-08 20:38:52 +0800

    Update solution for problem 5 using Ruby v1.9.2.

--- Prob5/5.rb
+++ Prob5/5.rb
@@ -1,15 +1,3 @@
 #!/usr/bin/ruby
 
-def gcd(a, b)
-  if b == 0
-    a
-  else 
-    gcd(b, a % b)
-  end
-end
-
-def lcm(a, b)
-  a / gcd(a, b) * b
-end
-
-puts (1..20).inject { |i, j| lcm(i, j) }
+puts (1..20).inject { |i, j| i.lcm(j) }
