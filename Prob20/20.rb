d9714278a441a56f0a3d461be8ac359f2f20c1a7
Daniel Le <GreenRecycleBin@gmail.com>
2011-11-05 20:55:49 +0800

    Modify solution to problem 20.

--- Prob20/20.rb
+++ Prob20/20.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/ruby
 
-num = (1..100).inject(1, :*)
 sum = 0
-num.to_s.each_char {|digit| sum += digit.to_i}
-puts sum
+(1..100).inject(:*).to_s.each_char { |digit| sum += digit.to_i }
+p sum
